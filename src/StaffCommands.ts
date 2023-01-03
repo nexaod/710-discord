18219d03ef239bbde5ca625ac59c6db5e19bd9ea
Nex AoD <114638787+nexaod@users.noreply.github.com>
2023-01-03 17:13:24 +0800

    Add table of contents

--- src/StaffCommands.ts
+++ src/StaffCommands.ts
@@ -20,7 +20,18 @@ client.on("ready", async () => {
     const channel = await client.channels.fetch(Channels.commandInfo) as TextChannel;
     await purgeChannel(channel);
     await channel.send('> __**Command Information**__');
+    const tocEmbed = new EmbedBuilder()
+        .setTitle('Table of Contents')
+        .setColor(Colours.lightblue)
+        .setFooter({ text: 'Last Updated' })
+        .setTimestamp();
+    const fields: any = [];
     for await (const command of staffCommandData) {
+        const tocField: any = {
+            name: command.title,
+            value: '',
+            inline: true
+        };
         await channel.send(`⬥ __**${command.title}**__`);
         let description = command.description;
         if (command.sections) {
@@ -32,7 +43,7 @@ client.on("ready", async () => {
             .setColor(Colours.gold)
             .setDescription(description);
         if (command.image) generalEmbed.setImage(command.image);
-        await channel.send({ embeds: [generalEmbed] });
+        const generalEmbedMessage = await channel.send({ embeds: [generalEmbed] });
         if (command.commands) {
             for await (const subCommand of command.commands) {
                 let subDescription = `> ${subCommand.description}`
@@ -44,10 +55,16 @@ client.on("ready", async () => {
                     .setColor(Colours.lightblue)
                     .setDescription(subDescription);
                 if (subCommand.image) subEmbed.setImage(subCommand.image);
-                await channel.send({ embeds: [subEmbed] });
+                const commandEmbed = await channel.send({ embeds: [subEmbed] });
+                tocField.value += `[${subCommand.title}](${commandEmbed.url})\n`
             }
+        } else {
+            tocField.value = `[Link](${generalEmbedMessage.url})`;
         }
+        fields.push(tocField);
     }
+    tocEmbed.setFields(fields);
+    await channel.send({ embeds: [tocEmbed] });
     await process.exit(0)
     console.log(`${client.user.username} is online`);
 });
