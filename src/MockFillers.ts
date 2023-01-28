b9f30117dfee3b1560478631921b94833cf60282
ethicalgaming <114638787+nexaod@users.noreply.github.com>
2023-01-29 00:35:14 +1100

    Update MockFillers.ts

--- src/MockFillers.ts
+++ src/MockFillers.ts
@@ -2,7 +2,7 @@ import { Client, EmbedBuilder, TextChannel } from "discord.js";
 import { Channels, purgeChannel, Roles, Colours } from "./Utils";
 import { mockFillerData } from "./data/MockFillers";
 
-const token = process.env.DISCORD_TOKEN; // add your token here
+const token = process.env.DISCORD_TOKEN;
 
 console.log("Bot is starting...");
 
